c089ebcacaaf6eb47dfae501c8519bae0640d51e
sjgridley <88527083+sjgridley@users.noreply.github.com>
2021-10-01 21:07:23 +1000

    Added House Class

--- app/models.py
+++ app/models.py
@@ -5,15 +5,19 @@ class Grade(db.Model):
     name = db.Column(db.Text)
     students = db.relationship('Student', backref='grade')
 
-# TODO: Add a class for the database model for the House table
+class House(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    colour = db.Column(db.Text)
+    students = db.relationship('Student', backref='house')
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
     grade_id = db.Column(db.Integer, db.ForeignKey('grade.id'), nullable=False)
-    # TODO: Add a house_id column, a foreign key to the House table, once the House model has been defined
+    house_id = db.Column(db.Text, db.ForeignKey('house.id'))
     english_mark = db.Column(db.Integer)
     science_mark = db.Column(db.Integer)
-    # TODO: Add a column for a student's mathematics mark
+    mathematics_mark = db.Column(db.Integer)
     does_homework = db.Column(db.Boolean)
-    # TODO: Add a column for a student's stay_on_task behaviour
+    stays_on_task = db.Column(db.Boolean)
+
